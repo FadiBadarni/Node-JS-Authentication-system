c7b594f87fe026ead61cfee0ac15cfe42876459b
Fadi <fadybd1@gmail.com>
2022-08-18 12:33:50 +0300

    Register + Login functionality

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
 const path = require("path");
 const mysql = require("mysql");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const session = require("express-session");
-require("dotenv").config();
 
-const encoder = bodyParser.urlencoded();
+const sequelize = require("./config/database");
 const webRoutes = require("./routes/web");
 const errorController = require("./app/controllers/ErrorController");
-
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_DATABASE = process.env.DB_DATABASE;
-const DB_PORT = process.env.DB_PORT;
-const connection = mysql.createPool({
-  connectionLimit: 100,
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(webRoutes);
-app.use(errorController.pageNotFound);
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(
   session({
-    secret: "secret-key",
+    cookie: { maxAge: 60000 },
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
 );
 
-connection.getConnection((err, connection) => {
-  if (err) throw err;
-  console.log("Data Base connected successfully: " + connection.threadId);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(webRoutes);
+app.use(errorController.pageNotFound);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT);
+    console.log("App listening on port " + process.env.PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
